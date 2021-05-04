147510267d197b3ca50632d26cef951066601363
Ahmet Susar <susar.eem@gmail.com>
2021-05-05 00:44:51 +0300

    dataset manipulations with pandas

---
+++ pandas_ile_veri_kumesi_islemleri.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# read_csv, csv uzantılı dosyayı okuyarak, DataFrame'e çevirir. 
+df = pd.read_csv('titanic.csv')
+
+# head(), DataFrame'in ilk 5 satırını döndürür.
+print(df.head())
+
+# describe(include='number'), sayısal verilerin olduğu sütunlar hakkında istatistik tablosu döndürür.
+# describe(include='object'), sayısal olmayan sütunlar hakkında istatistik tablosu döndürür.
+# describe(include='all'), tüm sütunlar hakkında istatistik tablosu döndürür.
+# describe() karma veri kümelerinde varsayılan olarak sayısal verilerin olduğu sütunlar hakkında istatistik tablosu döndürür.
+print(df.describe(include='number'))
