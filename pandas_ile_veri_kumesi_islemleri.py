1be6af59dd135bd67cba7fb8e4e850a880f8b4fb
Ahmet Susar <susar.eem@gmail.com>
2021-05-06 02:24:10 +0300

    Açıklama satırı değiştirildi.

--- pandas_ile_veri_kumesi_islemleri.py
+++ pandas_ile_veri_kumesi_islemleri.py
@@ -3,7 +3,7 @@ import pandas as pd
 # read_csv, csv uzantılı dosyayı okuyarak, DataFrame'e çevirir. 
 df = pd.read_csv('titanic.csv')
 
-# head(), DataFrame'in ilk 5 satırını döndürür.
+# head(), DataFrame'in ilk 5 satırını (başlık dahil) döndürür.
 print(df.head())
 
 # describe(include='number'), sayısal verilerin olduğu sütunlar hakkında istatistik tablosu döndürür.
